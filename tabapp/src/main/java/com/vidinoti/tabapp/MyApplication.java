66986ec2f3d8d0dc1c0c2b854c84b813f9dd3214
Johan Leuenberger <johan.leuenberger@bigtincan.com>
2024-01-30 16:44:40 +0100

    Add example app with bottom navigation view

---
+++ tabapp/src/main/java/com/vidinoti/tabapp/MyApplication.java
@@ -0,0 +1,25 @@
+package com.vidinoti.tabapp;
+
+import android.app.Application;
+
+import com.vidinoti.vdarsdk.VidinotiAR;
+import com.vidinoti.vdarsdk.VidinotiAROptions;
+import com.vidinoti.vdarsdk.VidinotiLanguage;
+
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        String licenseKey = "qv8db1pcnzc444ysnqtj";
+        // TODO Configure me
+        VidinotiAROptions options = new VidinotiAROptions.Builder(licenseKey)
+                .setCodeRecognitionEnabled(true)
+                .setSynchronizationMode(VidinotiAROptions.SynchronizationMode.LANGUAGE_ONLY)
+                .setMultilingualEnabled(true)
+                .setDefaultLanguage(VidinotiLanguage.EN)
+                .setSupportedLanguages(VidinotiLanguage.EN, VidinotiLanguage.FR)
+                .build();
+        VidinotiAR.init(this, options).synchronize();
+    }
+}
